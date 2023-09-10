37854d3083d48122b68adbc5afbbf40c4eb38263
Anton Curanz <anton@curanz.de>
2023-09-10 15:37:25 +0200

    backblaze region change

--- scripts/backuputil/common/constants.go
+++ scripts/backuputil/common/constants.go
@@ -1,5 +1,5 @@
 package common
 
-const S3Endpoint = "s3.us-east-005.backblazeb2.com"
+const S3Endpoint = "s3.eu-central-003.backblazeb2.com"
 const S3BucketDb = "homelab-db"
 const S3BucketK8up = "homelab-k8up"
